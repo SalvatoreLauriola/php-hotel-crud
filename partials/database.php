49146fb3a734fbf9abd35f431d4339acaac8aef3
Salvatore Lauriola <salvatore6324@gmail.com>
2020-05-28 17:00:59 +0200

    First step

---
+++ partials/database.php
@@ -0,0 +1,14 @@
+<?php
+
+@include __DIR__ . '/../env.php';
+
+//Connessione
+$conn = new mysqli ($server_name, $username, $password, $db_name);
+
+
+//Check connessione
+if ($conn && $conn->connect_error) {
+    die('Si è verificato un errore');
+}
+
+echo 'Hello db';
